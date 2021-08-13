c1045000ee471feea631c89ef93f3910017ee8f5
Raphael Tetreault <raphael.tetreault@outlook.com>
2021-08-13 18:42:37 -0400

    Many changes, mostly visual in nature
    JavaScript
    * Rename get-parts.js as randomizer.js
    * Remove constant literals with constant variables for table index
    * Switch to using (gx, ax, ex) tags
    
    HTML
    * Remove thead and contents
    * Remove tbody tags
    * Correct table data classes (gx, ax, ex)
    * Images point to 64px versions
    * Organize option buttons
    * Rename HTML to match repo name
    
    CSS
    * Copy base styles from personal portfolio
    * Style buttons
    * Style headings

---
+++ randomizer.js
@@ -0,0 +1,134 @@
+// Constants
+const kTableBodyIdx = 0;
+
+function rand(max){
+    var value = Math.random() * max;
+    var intValue = Math.floor(value);
+    return intValue;
+}
+
+function randomize(){
+    // Create array to iterate over
+    var tableIDs = ['body', 'cockpit', 'booster'];
+    // Iterate over each table
+    for (var index in tableIDs){
+        // Get string from array
+        var tableID = tableIDs[index];
+        // Get tables from DOM
+        var table = document.getElementById(tableID);
+        // Get indexes for rows which are enabled
+        var indexes = getCheckedRowIndexes(table);
+        // Get random sub-selection
+        var inputFieldID = 'input-' + tableID;
+        var total = document.getElementById(inputFieldID).value;
+        var subIndexes = getRandomSubSelection(indexes, total);
+        // Disable all rows, enable those selected
+        setRowsState(table, 'none');
+        setRowsStateByIndex(table, 'inherit', subIndexes);
+    }
+}
+
+function getCheckedRowIndexes(table){
+    //
+    var indexes = [];
+    //
+    var tableBody = table.children[kTableBodyIdx];
+    //
+    for (var i in tableBody.children){
+        // Getting a weird error where item() is last element returned? Skip it.
+        if (isNaN(i))
+            continue;
+
+        var tableRow = tableBody.children[i];
+        // Convert to zero-based index
+        var index = tableRow.children[0].innerText - 1;
+        // Checkbox is element 1 of table row. Input is sub element (0).
+        var checkbox = tableRow.children[1].children[0];
+
+        var isChecked = checkbox.checked;
+        if (isChecked){
+            indexes.push(index);
+            //console.log('checked: ' + index);
+        }
+    }
+    return indexes;
+}
+
+function setRowsState(table, state){
+    //
+    var tbody = table.children[kTableBodyIdx];
+    // Turn off all rows
+    for (var index in tbody.children){
+        // Getting a weird error where item is last element returned? Skip it.
+        if (isNaN(index))
+            continue;
+        //
+        var tableRow = tbody.children[index];
+        tableRow.style.display = state;
+    } 
+}
+
+function setRowsStateByIndex(table, state, indexes){
+    //
+    var tbody = table.children[kTableBodyIdx];
+    // Enable selected rows
+    for (var i in indexes){
+        var index = indexes[i];
+        var tableRow = tbody.children[index];
+        tableRow.style.display = state;
+    }
+}
+
+function getRandomSubSelection(indexes, total){
+    // Get min value from set
+    var min = total < indexes.length ? total : indexes.length;
+    // Clone array. We need to remove selected items from list as we iterate to prevent duplicates.
+    var indexesClone = [...indexes]
+    // Init parts array for randomly selected parts. Iterate for n parts, pick at random.
+    var subIndexes = [];
+    // 
+    for (var i = 0; i < min; i++){
+        // Get random index to select
+        var randomIndex = rand(indexesClone.length);
+        // Get index from list, add to selection
+        var index = indexesClone[randomIndex];
+        subIndexes.push(index);
+        // Remove item from clone list
+        indexesClone.splice(randomIndex, 1);
+    }
+    return subIndexes;
+}
+
+function setRowsCheckboxByClass(classID, isChecked){
+    var rows = document.getElementsByClassName(classID);
+    for (var i in rows){
+        // Getting a weird error where item is last element returned? Skip it.
+        if (isNaN(i))
+            continue;
+        //
+        var row = rows[i];
+        // Checkbox is element 1 of table row. Input is sub element (0).
+        var checkbox = row.children[1].children[0];
+        checkbox.checked = isChecked;
+    }
+}
+
+function setRowsChecked(isChecked){
+    setRowsCheckboxByClass('gx', isChecked);
+    setRowsCheckboxByClass('ax', isChecked);
+    setRowsCheckboxByClass('ex', isChecked);
+}
+
+function showHideTables(state){
+    // Create array to iterate over
+    var tableIDs = ['body', 'cockpit', 'booster'];
+    // Iterate over each table
+    for (var index in tableIDs){
+        // Get string from array
+        var tableID = tableIDs[index];
+        // Get tables from DOM
+        var table = document.getElementById(tableID);
+        // Disable all rows, enable those selected
+        setRowsState(table, state);
+    }
+}
